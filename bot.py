d9889c8b10c935fa3c0ca99688f13881377e3732
Jacob Allen <jacoballen785@gmail.com>
2023-01-24 15:26:56 +0000

    Fixed error where discord reports application did not respond despite a message being sent.

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ with open("./config.json", "r") as config_file:
 
 manager_role = config_data["manager_role"]
 suggestions_open = config_data["suggestions_open"]
+active_pool_max_items = config_data["active_pool_max_items"]
     
 #LOAD POOL DATA FROM movie_pool.json
 with open("./movie_pool.json", "r") as pool_file:
@@ -61,19 +62,55 @@ async def movie_suggest(ctx, *, movie:str):
         select.disabled = True
         selected_movie_id = select.values[0]
 
-        #CHECK IF THE POOL IS FULL
-        #CHECK IF USER HAS ALREADY MADE A SUGGESTION THATS IN THE POOL
-        #CHECK IF MOVIE IN SUGGESTION POOL ALREADY
-        #IF NOT ADD IT LOCALLY AND IN FILES
+        url = f"https://api.themoviedb.org/3/movie/{selected_movie_id}?api_key={TMDB_TOKEN_V3}&query={movie}"
+        movie_data = requests.get(url).json()
+
+        movie_poster_url = f"https://image.tmdb.org/t/p/w500{movie_data['poster_path']}"
+
+        if suggestions_open == False:
+            await interaction.response.edit_message(view=view)
+            await interaction.followup.send(f"Pool's Closed.")
+            return
+
+        if len(active_pool) >= active_pool_max_items:
+            await interaction.response.edit_message(view=view)
+            await interaction.followup.send(f"The pool is at capacity, wait for some space to open up.")
+            return
+
+        if selected_movie_id in active_pool:
+            await interaction.response.edit_message(view=view)
+            await interaction.followup.send(f"Someone else beat ya to it. That movie is already in the pool.")
+            return
+
+        #CHECK IF USER HAS REACHED THEIR SUGGESTION CAP
+
+        with open("./movie_pool.json", "r") as pool_file:
+            pool_data = json.load(pool_file)
+        
+        pool_data["active_pool"].append(selected_movie_id)
+
+        with open("./movie_pool.json", "w") as pool_file:
+            json.dump(pool_data, pool_file, indent=4, separators=(',',': '))
+
+        response_embed = discord.Embed(
+            title=f"Movie added to the pool.",
+            description=f"Congrats! {movie_data['title']} has been added to the pool. Good luck in the draw.",
+            url=movie_data["homepage"]
+        )
+
+        response_embed.add_field(name="Rating", inline=True, value=f"{int(float(movie_data['vote_average'])*10)}%")
+        response_embed.add_field(name="Original Title", inline=True, value=movie_data['original_title'])
+        response_embed.add_field(name="Original Language", inline=True, value=movie_data['original_language'])
+        response_embed.set_thumbnail(url=movie_poster_url)
 
         await interaction.response.edit_message(view=view)
-        await interaction.followup.send(f"Awesome! I like {select.values[0]} too!")
+        await interaction.followup.send(embed=response_embed)
 
     select.callback = movie_select_callback
 
     view.add_item(select)
 
-    await ctx.send("hi", view=view)
+    await ctx.respond("Search complete please select the correct movie below. (if it isn't on there ensure the title is correct)", view=view)
 
 @bot.slash_command(name='draw', description='Randomly selects a movie from the pool, creates an event and messages the movie channel.')
 async def draw(ctx):
